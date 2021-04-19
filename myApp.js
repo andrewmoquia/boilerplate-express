e544c276e566b1ec9cd8d8dac5b22f50a57d3244
andrewmoquia <andrewmoquia09@gmail.com>
2021-04-19 16:26:03 +0800

    Install npm dotenv

--- myApp.js
+++ myApp.js
@@ -2,16 +2,28 @@ var express = require('express');
 var app = express();
 
 console.log("Hello World");
-
-app.use('/public', express.static(__dirname +  '/public'));
-
+//add the index.html to our server to display for the client
 app.get("/", (req,res) => {
   res.sendFile(__dirname + "/views/index.html");
-})
+});
 
-app.get('/json', (req,res) => {
-    res.json({"message":"Hello json"})
-})
+//add some files like css or image the server that will support the index.html
+app.use('/public', express.static(__dirname +  '/public'));
+
+//add json route to the website with a dummy object
+// app.get('/json', (req,res) => {
+//     res.json({"message":"Hello json"})
+// });
+
+//use .env to configure private data
+//we need to install npm install dotenv --save
+app.get("/json", (req,res) => {
+    if (process.env.MESSAGE_STYLE === "uppercase") {
+        res.json({"message": "HELLO JSON"})
+    } else {
+        res.json({"message": "Hello json"})
+    }
+});
 
 
 
